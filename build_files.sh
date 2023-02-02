5f4fa561ccd6007862275ffff158427202471b3e
IgorBekchruin <worki.iba@gmail.com>
2023-02-02 09:02:46 +0400

    mod build.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,9 @@
 # build_files.sh
 
-echo "Building project..."
 pip install -r requirements.txt
 
-echo "Make migration"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 
-echo "Collect static"
-python3.9 manage.py collectstatic --noinput --clear
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
+
+python3.9 manage.py collectstatic --noinput
