b00c157f5a978222a6a8b1eb3ba0fcbadeac7552
IgorBekchruin <worki.iba@gmail.com>
2023-02-02 09:43:48 +0400

    edit urls, versel.jason

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('', include('news.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
